3dc8728415b778e0993a396de0da97d1efe28f27
thesiddharth24 <siddharth20it028@gmail.com>
2024-02-15 17:46:21 +0530

    complete project

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../../main";
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaYoutube, FaLinkedin  , FaGitHub} from "react-icons/fa";
+import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
@@ -11,7 +11,7 @@ const Footer = () => {
       <div>&copy; All Rights Reserved By Siddharth.</div>
       <div>
         <Link to={"https://github.com/thesiddharth24"} target="_blank">
-          <FaGitHub />
+          {/* <FaGitHub /> */}
         </Link>
         <Link to={"https://www.linkedin.com/in/siddharth-g-103003110/"} target="_blank">
           <FaLinkedin />
